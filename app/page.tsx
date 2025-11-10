ba3a92af349d53342c52f29eb7b2ddd5e2fb0310
farukukWD <farukgb1999@gmail.com>
2025-11-11 00:12:06 +0600

    init

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex justify-center items-center flex-col">
+      <h1>Hello, Welcome to smart tax bd</h1>
+    </main>
+  );
+}
