54e4cd05ce1c58bc45b258b8b21d26b580b75daf
farukukWD <farukgb1999@gmail.com>
2026-02-18 01:35:43 +0600

    feat: Introduce tax type selection on the homepage and enhance order creation form with pre-filled user data and query parameter integration.

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,11 @@
 import HeroBanner from "@/components/hero-section/hero-banner";
+import TaxesTypes from "@/components/taxes/taxes-types";
 
 export default function Home() {
   return (
-    <main>
+    <main className="space-y-4">
       <HeroBanner />
+      <TaxesTypes />
     </main>
   );
 }
