46df59250d1acd4853adc1ce98c7a4b1f2d1c645
farukulWD <farukgb1999@gmail.com>
2026-06-20 09:12:03 +0600

    feat: add Clarity script for enhanced user analytics in layout component

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Noto_Sans_Bengali } from "next/font/google";
+import Script from "next/script";
 import "../globals.css";
 import ReduxProvider from "@/components/layouts/redux-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
@@ -69,6 +70,15 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
+      <head>
+        <Script
+          id="ms-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","x9schu08e5");`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${notoSansBengali.variable} antialiased`}
       >
